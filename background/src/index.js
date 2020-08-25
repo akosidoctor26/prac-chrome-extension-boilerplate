c51c4531b1a9379da7e5be6ab8dc652587d1c053
Kristine Doc <mskristinedoctor@gmail.com>
2020-08-25 13:09:51 -0700

    cleanup

--- background/src/index.js
+++ background/src/index.js
@@ -1,15 +1,1 @@
 console.log('background.js');
-
-class Background {
-  constructor() {}
-}
-
-const tout = () => {
-  setTimeout(() => {
-    console.log('test');
-  }, 100);
-};
-
-tout();
-
-var bg = new Background();
